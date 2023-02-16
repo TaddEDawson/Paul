3706714770f6068e20f6ee912d9478542b9a4800
Tadd E. Dawson <tadd@microsoft.com>
2023-02-16 14:48:53 -0500

    Initial Commit

---
+++ Remove-Logs.ps1
@@ -0,0 +1,15 @@
+<#
+    .SYNOPSIS
+        Removes files older than a given date in a specifc folder
+    .EXAMPLE
+        C:\demo\Paul\Current\Remove-Logs.ps1 -Verbose
+#>
+[CmdletBinding()]
+param
+(
+
+) #param
+process
+{
+
+} # process
